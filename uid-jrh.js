7ae47e2eaf4a7be4ed4c9752be1512829bcd3ebb
AXO <wasd160104@gmail.com>
2022-06-01 19:29:15 +0700

    nambahin uid-jrh, mhki-jhki

--- uid-jrh.js
+++ uid-jrh.js
@@ -7,134 +7,137 @@ const dotenv = require("dotenv");
 const AWS = require("aws-sdk");
 const fse = require("fs-extra")
 
-// ambil data dari yang sudah kita buat di file .env
 dotenv.config();
 const downloadPath = path.resolve(__dirname + './data');
 const spacesEndpoint = process.env.DO_SPACES_ENDPOINT;
 
-// Buat sambungan dengan s3 bucket punya heylaw, dengan data yang dari .env.
 const s3 = new AWS.S3({endpoint: spacesEndpoint, accessKeyId: process.env.DO_SPACES_KEY, secretAccessKey: process.env.DO_SPACES_SECRET});
 
-// buat fungsi asynchronous untuk puppeteer
 async function getDataJurnal(){
-	// Jalankan puppeteer pake .launch()
-	const browser = await puppeteer.launch({headless: true});
+	const browser = await puppeteer.launch({headless: false});
 
-	// Buat halaman baru pake .newpage()
 	const page = await browser.newPage();
 
-	// Arahkan halaman ke url yang diinginkan
-	await page.goto("https://ojs.uid.ac.id/index.php/jrh", {timeout: 0});
+	await page.goto("https://ojs.uid.ac.id/index.php/jrh/issue/archive", {timeout: 0})
 
-	// ambil semua link yang perlu kita ambil datanya pake .$$eval
-	const linkData = await page.$$eval(".title a", (link) => {
-		return link.map( x => x.href);
+	const linkIssue = await page.$$eval("a.title", (link) => {
+		return link.map(x => x.href)
 	})
 
-	const nama_jurnal = "Reformasi Hukum"//await page.$eval(".data strong", text => text.innerText)
 
-	const tahun = "2021" //await page.$eval(".current_issue_title", text => text.innerText.split(" ").pop())
+	let arr_nama_file = [];
 
-	let arr_nama_file = []
+	let linkToDownload = [];
 
-	for(let i = 0; i < linkData.length; i++){
-		await page.goto(linkData[i], {timeout: 0});
+	const nama_jurnal = "Reformasi Hukum "//await page.$eval(".data strong", text => text.innerText)
 
-		const title = await page.$eval(".page_title", title => title.innerText);
+	const universitas = "Universitas Islam Jakarta";
+	
+	for(let i = 0; i < linkIssue.length; i++){
+		await page.goto(linkIssue[i], {timeout: 0})
 
-		const linkPDF = await page.$eval(".pdf", href => href.href)
+		const linkData = await page.$$eval(".title a", (link) => {
+			return link.map( x => x.href);
+		})
+
+		const tahun = await page.$eval(".published .value", text => text.innerText.split("-").shift())
 
 
-		// Buat try catch buat nampung jika data tidak ada, maka akan diisi "-"
-		let universitas;
-		try {
-			universitas = await page.$eval(".affiliation", text => text.innerText)
-		} catch {
-			universitas = "-"
-		}
+		for(let j = 0; j < linkData.length; j++){
+			await page.goto(linkData[j], {timeout: 0});
 
-		let abstrak;
-		try {
-			abstrak = await page.$eval(".abstract p", text => text.innerText)
-		} catch {
-			abstrak = "-"
-		}
+			const title = await page.$eval(".page_title", title => title.innerText);
 
-		let author;
-		try {
-			author = await page.$$eval(".name", (name) =>{
-				return name.map(text => text.innerText).join(", ") 
+			const linkPDF = await page.$eval(".obj_galley_link", href => href.href)
+
+			linkToDownload.push(linkPDF);
+
+
+			// Buat try catch buat nampung jika data tidak ada, maka akan diisi "-"
+			let abstrak;
+			try {
+				abstrak = await page.$$eval(".abstract p", (abstrak) => {
+					return abstrak.map(text => text.innerText).join("\n")
+				})
+			} catch {
+				abstrak = "-"
+			}
+
+			let author;
+			try {
+				author = await page.$$eval(".name", (name) =>{
+					return name.map(text => text.innerText).join(", ") 
+				});
+			} catch {
+				author = "-"
+			}
+
+			let doi;
+			try {
+				doi = await page.$eval(".doi .value", text => text.innerText)
+			} catch {
+				doi = "-"
+			}
+
+
+			title_file = title.split(" ", 10).join(" ");
+
+			author_file = author.split(",").shift();
+
+			let nama_file = `${author_file} - ${title_file}.pdf`
+
+			nama_file = nama_file.replace(/[\/\\\?:*?"<>|()]/gi, "")
+
+			arr_nama_file.push(nama_file);
+
+			const dirtyData = {title, author, nama_jurnal, universitas, doi, abstrak, nama_file, tahun};
+
+			const data = {}
+
+			for (var x in dirtyData) {
+			  data[x] = dirtyData[x].replace(/["]/g,'');
+			}
+			console.log(data)
+			// Masukkan data ke database
+			let con = mysql.createConnection({
+			  host: "localhost",
+			  user: "root",
+			  password: "",
+			  database: "tugas mas nandi"
 			});
-		} catch {
-			author = "-"
+
+			con.connect(function(err) {
+			  if (err) throw err;
+
+			  let sql = `INSERT INTO crawler_jurnal (title, author, nama_jurnal, universitas, doi, abstrak, nama_file, tahun) VALUES ("${data.title}", "${data.author}", "${data.nama_jurnal}", "${data.universitas}", "${data.doi}", "${data.abstrak}", "${data.nama_file}", "${data.tahun}") `;
+
+			  con.query(sql, function(err, result){
+			    if(err) throw err;
+
+			    console.log(`berhasil menambahkan ${j+1} row`)
+			  })
+
+	  		con.end()
+
+			});
+
+			console.log(linkToDownload)
 		}
-
-		let doi;
-		try {
-			doi = await page.$eval(".doi .value", text => text.innerText)
-		} catch {
-			doi = "-"
-		}
-
-		
-		// Karena nama file gak bisa banyak-banyak characternya maka kita rapihkan di sini.
-
-		// untuk title di nama file nya kita buat agar tidak lebih dari 10 kata
-		title_file = title.split(" ", 10).join(" ");
-
-		// untuk author di nama file nya kita buat hanya satu penulis saja.
-		author_file = author.split(",").shift();
-
-		// Lalu kita susun untuk nama file nya agar menjadi satu dan di belakangnya kita tambah .pdf
-		let nama_file = `${author_file} - ${title_file}.pdf`
-
-		// hapus character yang ada di dalam nama_file menggunakan regex, agar saat di-rename tidak menimbulkan error
-		nama_file = nama_file.replace(/[\/\\\?:*?"<>|()]/gi, "")
-
-		// masukkan nama-nama filenya ke dalam array
-		arr_nama_file.push(nama_file);
-
-		const data = {title, author, nama_jurnal, universitas, doi, abstrak, nama_file, tahun};
-
-		console.log(data)
-		// Masukkan data ke database
-		let con = mysql.createConnection({
-		  host: "localhost",
-		  user: "root",
-		  password: "",
-		  database: "tugas mas nandi"
-		});
-
-		con.connect(function(err) {
-		  if (err) throw err;
-
-		  let sql = `INSERT INTO crawler_jurnal (title, author, nama_jurnal, universitas, doi, abstrak, nama_file, tahun) VALUES ("${data.title}", "${data.author}", "${data.nama_jurnal}", "${data.universitas}", "${data.doi}", "${data.abstrak}", "${data.nama_file}", "${data.tahun}") `;
-
-		  con.query(sql, function(err, result){
-		    if(err) throw err;
-
-		    console.log(`berhasil menambahkan ${i+1} row`)
-		  })
-
-  		con.end()
-
-		});
-
-		await page._client.send('Page.setDownloadBehavior', {
-            behavior: 'allow',
-            downloadPath: downloadPath,
-
-        });
-
-		await page.goto(linkPDF, {timeout: 0})
-
-		await page.click(".download")
-
-		console.log(`Berhasil mendownload ${i+1} file`)
-
-
 	}
 
+	for(let i = 0; i < linkToDownload.length; i++){
+
+		await page._client.send('Page.setDownloadBehavior', {
+	            behavior: 'allow',
+	            downloadPath: downloadPath,
+
+	        });
+
+				await page.goto(linkToDownload[i], {timeout: 0})
+				await page.click(".download")
+				console.log(`Berhasil mendownload ${i+1} file`)
+	}
+	
 	await page.waitForTimeout(30000);
 
 
